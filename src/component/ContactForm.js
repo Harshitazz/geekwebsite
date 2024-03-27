bb61f3fc8d88f0e8fe6ce65d6f4f42a82ca17006
Aditya <adityakumarji74@gmail.com>
2024-03-27 14:20:10 +0530

    email validation

--- src/component/ContactForm.js
+++ src/component/ContactForm.js
@@ -1,13 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 import "./ContactForm.css";
 
 export default function ContactForm() {
-  const submitHandler=()=>{
-    console.log('submit');
-  }
+  const [email, setEmail] = useState("");
+  const [emailValid, setEmailValid] = useState(true); // Initially assuming email is valid
+
+  const handleEmailChange = (e) => {
+    const inputValue = e.target.value;
+    setEmail(inputValue);
+
+    // Regular expression for validating email format
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+    // If input is not empty, test against the regex pattern
+    // Otherwise, reset the email validity to true
+    setEmailValid(
+      inputValue.trim() === "" ? true : emailRegex.test(inputValue)
+    );
+  };
+
+  const submitHandler = () => {
+    console.log("submit");
+  };
+
   return (
     <div className="group-3">
-      <form className="contact-form-details" >
+      <form className="contact-form-details">
         <div className="section-heading ">
           <p className="about-us ">Contact Form</p>
           <div className="underline"></div>
@@ -24,14 +42,20 @@ export default function ContactForm() {
             <div class="sphere2" style={{ left: "-45%" }}></div>
           </div>
           <div className="frame-12">
-            <input placeholder="EMAIL" className="info" />
+            <input
+              placeholder="EMAIL"
+              className={`info ${!emailValid ? "invalid" : ""}`}
+              onChange={handleEmailChange}
+            />
           </div>
           <div className="frame-14">
             <input placeholder="MESSAGE" className="info" />
           </div>
         </div>
 
-        <button className="submit" onClick={submitHandler}>Submit</button>
+        <button className="submit" onClick={submitHandler}>
+          Submit
+        </button>
       </form>
 
       <div className="group-892">
